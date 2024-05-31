b08be76f30ccf9e8d7ab932432d9c1e536fc060a
EspositoLucas <espositolucas2002@gmail.com>
2024-05-30 23:01:40 -0300

    agrego estructura base de repositorio

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='',
+    version='1.0',
+    packages=['src', 'src.utils'],
+    url='',
+    license='MIT',
+    author='',
+    author_email='',
+    description='An example of a basic Python repository',
+    install_requires=[
+        'requests==2.20.0'
+    ]
+)
