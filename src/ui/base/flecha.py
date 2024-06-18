5bf212adadfe7efe6a428075ae8da925770a4c4e
EspositoLucas <espositolucas2002@gmail.com>
2024-06-18 16:58:47 -0300

    agrego que flechas se muevan e intensifico la punta de la flecha

--- src/ui/base/flecha.py
+++ src/ui/base/flecha.py
@@ -13,13 +13,16 @@ import math
 #                        arrow_height 
 
 class Flecha(QtWidgets.QGraphicsItem):
-    def __init__(self, source: QtCore.QPointF, destination: QtCore.QPointF, arrow_height, arrow_width, length_width, *args, **kwargs):
+    def __init__(self, source: QtCore.QPointF, destination: QtCore.QPointF, arrow_height=15, arrow_width=10, length_width=5, *args, **kwargs):
         super(Flecha, self).__init__(*args, **kwargs)
         self._sourcePoint = source
         self._destinationPoint = destination
         self._arrow_height = arrow_height
         self._arrow_width = arrow_width
         self._length_width = length_width
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsFocusable, True)
+        self.setAcceptHoverEvents(True)  # Aceptar eventos de hover para cambiar el cursor
 
     def boundingRect(self):
         extra = 10
@@ -30,13 +33,15 @@ class Flecha(QtWidgets.QGraphicsItem):
         painter.setRenderHint(QtGui.QPainter.Antialiasing)
 
         my_pen = QtGui.QPen()
-        my_pen.setWidth(1)
+        my_pen.setWidth(2)  # Hacer la flecha más fina
         my_pen.setCosmetic(False)
-        my_pen.setColor(QtGui.QColor(255, 0, 0, 100))
+        my_pen.setColor(QtGui.QColor(255, 0, 0))
         painter.setPen(my_pen)
 
         arrow_polygon = self.arrowCalc()
         if arrow_polygon is not None:
+            painter.drawPolygon(arrow_polygon)
+            painter.setBrush(QtGui.QColor(255, 0, 0))  # Pintar el interior de la flecha
             painter.drawPolygon(arrow_polygon)
 
     def arrowCalc(self, start_point=None, end_point=None):
@@ -52,8 +57,8 @@ class Flecha(QtWidgets.QGraphicsItem):
             perpX = -normY
             perpY = normX
             
-            point2 = endPoint + QtCore.QPointF(normX, normY) * self._arrow_height + QtCore.QPointF(perpX, perpY) * self._arrow_width
-            point3 = endPoint + QtCore.QPointF(normX, normY) * self._arrow_height - QtCore.QPointF(perpX, perpY) * self._arrow_width
+            point2 = endPoint + QtCore.QPointF(normX, normY) * self._arrow_height * 5 + QtCore.QPointF(perpX, perpY) * self._arrow_width * 5  # Intensificar la punta
+            point3 = endPoint + QtCore.QPointF(normX, normY) * self._arrow_height * 5 - QtCore.QPointF(perpX, perpY) * self._arrow_width * 5  # Intensificar la punta
 
             point4 = startPoint + QtCore.QPointF(perpX, perpY) * self._length_width
             point5 = endPoint + QtCore.QPointF(normX, normY) * self._arrow_height + QtCore.QPointF(perpX, perpY) * self._length_width
@@ -64,3 +69,16 @@ class Flecha(QtWidgets.QGraphicsItem):
 
         except (ZeroDivisionError, Exception):
             return None
+
+    def itemChange(self, change, value):
+        if change == QtWidgets.QGraphicsItem.ItemPositionChange:
+            new_pos = value
+            self._sourcePoint = new_pos
+            self._destinationPoint = new_pos + QtCore.QPointF(100, 100)  # Ajusta los valores según sea necesario
+        return super(Flecha, self).itemChange(change, value)
+
+    def hoverEnterEvent(self, event):
+        self.setCursor(QtCore.Qt.CrossCursor)  # Cambiar el cursor cuando el mouse esté sobre la flecha
+
+    def hoverLeaveEvent(self, event):
+        self.setCursor(QtCore.Qt.ArrowCursor)  # Restablecer el cursor cuando el mouse salga de la flecha
