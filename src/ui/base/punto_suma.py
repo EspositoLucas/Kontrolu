cf37cb8f8b83b8130694dcf52d73ce1a252f8883
EspositoLucas <espositolucas2002@gmail.com>
2024-06-18 14:40:30 -0300

    corrijo borde gris de punto suma para que no se vea y su tamanio

--- src/ui/base/punto_suma.py
+++ src/ui/base/punto_suma.py
@@ -1,17 +1,18 @@
 import os
 from .macro_vista import MacroVista
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class PuntoSuma(MacroVista):
     def __init__(self, puntoSuma):
         MacroVista.__init__(self, puntoSuma, QtCore.QRect(140, 205, 51, 51))
         self.setText("") # el punto suma no tiene texto
         self.setDisabled(True) # el punto suma no se puede apretar
-        
+        self.setStyleSheet("background-color: transparent;") # Establecer fondo transparente
+
         # ICONO
         icon = QtGui.QIcon()
         path = os.path.dirname(os.path.abspath(__file__))
         image_path = os.path.join(path, 'imgs', 'puntoSuma.png')
         icon.addPixmap(QtGui.QPixmap(image_path), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.setIcon(icon)
-        self.setIconSize(QtCore.QSize(50, 50))
+        self.setIconSize(QtCore.QSize(51, 51)) # Ajustar el tamaño del icono al tamaño del botón
