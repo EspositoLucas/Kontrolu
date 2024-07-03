97aa595279dc96ca94f7fc8473b7dc4bcde60411
SantiagoDemattei <sdemattei@frba.utn.edu.ar>
2024-07-03 18:29:37 -0300

    dibujado de paralelos funcionando

--- src/ui/base/drawing_area.py
+++ src/ui/base/drawing_area.py
@@ -25,6 +25,7 @@ class DrawingArea(QWidget):
                 microbloque.deleteLater() # elimina cada elemento
         self.microbloques.clear() # vacia la lista de microbloques
         self.dibujar_topologia(self.modelo.topologia, QPointF(150, self.height() / 2))
+        #self.print_topologia(self.modelo.topologia)
         self.update()
     
     def dibujar_topologia(self, topologia, posicion_inicial):
@@ -42,10 +43,16 @@ class DrawingArea(QWidget):
             posicion_actual.setX(posicion_actual.x() + 200) # TODO: Modificar el valor segun convenga (es el margen horizontal entre microbloques)
 
     def dibujar_paralelo(self, paralelo, posicion_inicial):
-        posicion_actual = posicion_inicial
+        altura_total = sum(hijo.alto() for hijo in paralelo.hijos) # la altura total de los microbloques es la suma de sus alturas (porque se dibujan uno debajo del otro)
+        vertical_margin = 50  # TODO: Modificar el valor segun convenga (es el margen vertical entre microbloques)
+        altura_total += (len(paralelo.hijos) - 1) * vertical_margin # se agrega el margen vertical entre microbloques
+
+        y_actual = posicion_inicial.y() - altura_total / 2 # empiezo desde la mitad de la altura total
         for hijo in paralelo.hijos:
-            self.dibujar_topologia(hijo, posicion_actual)
-            posicion_actual.setY(posicion_actual.y() + 100) # TODO: Modificar el valor segun convenga (es el margen vertical entre microbloques)
+            centro_del_hijo = y_actual + hijo.alto() / 2 # centro del microbloque
+            posicion_del_hijo = QPointF(posicion_inicial.x(), centro_del_hijo)
+            self.dibujar_topologia(hijo, posicion_del_hijo)
+            y_actual += hijo.alto() + vertical_margin # la posicion del siguiente será más abajo
 
     def create_microbloque(self, microbloque_back, pos):
         microbloque = Microbloque(self, microbloque_back)
@@ -73,7 +80,16 @@ class DrawingArea(QWidget):
         if not self.microbloques:
             self.draw_empty_connection(painter)
         else:
-            self.draw_connections(painter)
+           punto_final = self.draw_connections(painter, self.modelo.topologia, QPointF(90, self.height() / 2))
+           self.draw_final_connection(painter, punto_final) # punto_final es el punto de salida de la última conexión
+
+    def draw_final_connection(self, painter, start_point):
+        if start_point is None:
+            return
+
+        end_point = QPointF(self.width() - 170, self.height() / 2) # end_point es el lugar donde está el bloque de salida
+        painter.setPen(QPen(Qt.black, 2))
+        painter.drawLine(start_point, end_point)
 
     def draw_empty_connection(self, painter):
         painter.setPen(QPen(Qt.black, 2))
@@ -108,24 +124,104 @@ class DrawingArea(QWidget):
         painter.drawEllipse(QPointF(centro_salida_x, centro_y), radio, radio)
         painter.drawText(QRectF(self.width()-209, self.height() / 2 - 30, 80, 60), Qt.AlignCenter, "Salida")
     
-    def draw_connections(self, painter):
-        painter.setPen(QPen(Qt.black, 2))
-        for i in range(len(self.microbloques) - 1):
-            start = self.microbloques[i].pos() + QPoint(self.microbloques[i].width(), self.microbloques[i].height() // 2)
-            end = self.microbloques[i+1].pos() + QPoint(0, self.microbloques[i+1].height() // 2)
-            painter.drawLine(start, end)
+    def draw_connections(self, painter, topologia, punto_de_partida, is_parallel=False):
+        if punto_de_partida is None:
+            return None
 
-        # Conectar el primer microbloque con la entrada
-        if self.microbloques:
-            entrada = QPointF(90, self.height() / 2)
-            primer_micro = self.microbloques[0].pos() + QPoint(0, self.microbloques[0].height() // 2)
-            painter.drawLine(entrada, primer_micro)
+        painter.setPen(QPen(Qt.black, 2)) # configura el color y grosor de la línea
+        
+        if isinstance(topologia, TopologiaSerie):
+            return self.draw_serie_connections(painter, topologia, punto_de_partida)
+        elif isinstance(topologia, TopologiaParalelo):
+            return self.draw_paralelo_connections(painter, topologia, punto_de_partida)
+        elif isinstance(topologia, MicroBloque):
+            return self.draw_microbloque_connection(painter, topologia, punto_de_partida, is_parallel)
+        else:
+            return punto_de_partida
 
-        # Conectar el último microbloque con la salida
-        if self.microbloques:
-            salida = QPointF(self.width() - 170, self.height() / 2)
-            ultimo_micro = self.microbloques[-1].pos() + QPoint(self.microbloques[-1].width(), self.microbloques[-1].height() // 2)
-            painter.drawLine(ultimo_micro, salida)
+    def draw_serie_connections(self, painter, serie, punto_inicial):
+        """
+        Dibuja las conexiones de una serie de microbloques
+        @return el punto final de la serie
+        """
+        if punto_inicial is None:
+            return None
+
+        punto_actual = punto_inicial
+        for hijo in serie.hijos:
+            # para cada elemento de la serie, llama a draw_connections para que dibuje sus componentes (si las hubiera)
+            punto_final = self.draw_connections(painter, hijo, punto_actual) 
+            punto_actual = punto_final
+
+        return punto_actual
+
+    def draw_paralelo_connections(self, painter, paralelo, punto_inicial):
+        if punto_inicial is None:
+            return None
+
+        # Calcular punto de bifurcación
+        comienzo_de_rama = QPointF(punto_inicial.x() + 20, punto_inicial.y())  # 20 pixels antes del bloque (es el margen antes de hacer la bifurcación)
+        altura_total = sum(hijo.alto() for hijo in paralelo.hijos) # idem que en dibujar_paralelo
+        vertical_margin = 50 # idem que en dibujar_paralelo
+        altura_total += (len(paralelo.hijos) - 1) * vertical_margin # idem que en dibujar_paralelo
+        
+        # Dibujar línea horizontal antes de la bifurcación
+        painter.drawLine(punto_inicial, comienzo_de_rama)
+        
+        # Dibujar conexiones para cada rama
+        y_actual = punto_inicial.y() - altura_total / 2
+        puntos_finales = []
+        for hijo in paralelo.hijos:
+            punto_final_rama_vertical = QPointF(comienzo_de_rama.x(), y_actual + hijo.alto() / 2) # deja igual la "x" y pone la "y" en el centro del microbloque
+            painter.drawLine(comienzo_de_rama, punto_final_rama_vertical)  # Línea vertical de bifurcación
+            punto_final_rama_actual = self.draw_connections(painter, hijo, punto_final_rama_vertical, True) # partiendo desde el extremo de la linea vertical de bifurcacion, comienza a dibujar lo que sigue
+            if punto_final_rama_actual is not None:
+                puntos_finales.append(punto_final_rama_actual) # se va guardando los puntos finales de cada rama del paralelo
+            y_actual += hijo.alto() + vertical_margin # incorpora el margen vertical
+        
+        if not puntos_finales: # querria decir que no dibujo nada en los paralelos (no creo que pase nunca, pero por las dudas lo dejo)
+            return punto_inicial
+
+        margen_horizontal_final = 30 # margen horizontal para salir del paralelo
+        # Encontrar el punto final más a la derecha
+        max_x = max(point.x() for point in puntos_finales) + margen_horizontal_final # esto está por si una rama quedó "mas larga horizontalmente" que la otra
+         
+        # Dibujar líneas horizontales para reconectar las ramas
+        for end_point in puntos_finales:
+            painter.drawLine(end_point, QPointF(max_x, end_point.y())) # le sumamos 20 para tener una linea horizontal (al salir del microbloque) antes de reconectar
+        
+        # Punto de reconexión
+        punto_de_reconexion = QPointF(max_x, punto_inicial.y())
+        
+        # Dibujar líneas verticales para reconectar (En realidad es una unica linea desde una rama a la otra)
+        # QPointF(max_x + 20, puntos_finales[0].y()) --> es para la rama de arriba (indice 0 es el primer elemento de una lista)
+        # QPointF(max_x + 20, puntos_finales[-1].y()) --> es para la rama de abajo (indice -1 es el último elemento de una lista)
+        painter.drawLine(QPointF(max_x, puntos_finales[0].y()), QPointF(max_x, puntos_finales[-1].y()))
+        
+        # Dibujar línea horizontal final (para salir de la estructura paralelo)
+        painter.drawLine(QPointF(max_x, punto_inicial.y()), punto_de_reconexion)
+        
+        # retorna el punto de reconexión porque es el punto "mas a la derecha" de la estructura paralelo
+        return punto_de_reconexion 
+
+    def draw_microbloque_connection(self, painter, microbloque, punto_inicial, es_paralelo):
+        # busca en la lista de microbloques de la drawing_area, el microbloque que queremos conectar
+        for mb in self.microbloques:
+            if mb.elemento_back == microbloque: # compara su elemento back
+                if es_paralelo:
+                    punto_final = QPointF(mb.pos().x(), punto_inicial.y()) # deja igual el "x", y el "y" lo deja igual respecto a de la rama paralela en donde está
+                else:
+                    punto_final = mb.pos() + QPointF(0, mb.height() / 2) # mb.pos() = da la esquina superior izquierda del microbloque ||| + QPointF(0, mb.height() / 2) = mueve el punto hacia abajo hasta la mitad de la altura del microbloque.
+                
+                # punto_final seria el punto en donde va a llegar la flecha que proviene del microbloque anterior (punto medio izquierdo del microbloque actual)
+
+                if punto_inicial is not None and punto_final is not None:
+                    painter.drawLine(punto_inicial, punto_final) # dibuja la linea
+                
+                return mb.pos() + QPointF(mb.width(), mb.height() / 2) # retorna un punto que representa la mitad del lado derecho del microbloque. Este punto se usará como punto de inicio para la siguiente conexión.
+        
+        # Si no se encuentra el microbloque, retornamos el punto de inicio --> Si no encuentra el microbloque en la lista, simplemente retorna el punto_inicial
+        return punto_inicial
 
     def create_new_microbloque(self, pos, relation=None, reference_microbloque=None):
         dialog = QDialog(self)
@@ -234,3 +330,19 @@ class DrawingArea(QWidget):
                 relation = 'despues'
             
             self.create_new_microbloque(self.selected_microbloque.pos(), relation, self.selected_microbloque)    
+
+    def print_topologia(self, topologia, indent=0):
+        """
+        Imprime el arbol del macrobloque por consola
+        """
+        space = ' ' * (indent * 2)
+        if isinstance(topologia, TopologiaSerie):
+            print(f"{space}Serie:")
+            for hijo in topologia.hijos:
+                self.print_topologia(hijo, indent + 1)
+        elif isinstance(topologia, TopologiaParalelo):
+            print(f"{space}Paralelo:")
+            for hijo in topologia.hijos:
+                self.print_topologia(hijo, indent + 1)
+        elif isinstance(topologia, MicroBloque):
+            print(f"{space}MicroBloque: {topologia.nombre}")
