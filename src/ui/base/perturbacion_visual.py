94f78aefd12f90c0ad5729ed82176989936d2e70
SantiagoDemattei <sdemattei@frba.utn.edu.ar>
2024-10-04 09:11:30 -0300

    fix: simbolo perturbación y estilos de dialogs

--- src/ui/base/perturbacion_visual.py
+++ src/ui/base/perturbacion_visual.py
@@ -1,25 +1,89 @@
-from PyQt5.QtWidgets import QGraphicsEllipseItem, QMenu, QAction, QMessageBox, QDialog, QVBoxLayout, QLabel, QSpinBox, QHBoxLayout, QPushButton, QGraphicsItem, QGraphicsSceneContextMenuEvent
-from PyQt5.QtGui import QBrush, QColor, QPen
+from PyQt5.QtWidgets import (
+    QGraphicsEllipseItem, 
+    QMenu, 
+    QAction, 
+    QMessageBox, 
+    QDialog, 
+    QVBoxLayout, 
+    QLabel, 
+    QSpinBox, 
+    QHBoxLayout, 
+    QPushButton, 
+    QGraphicsItem, 
+    QGraphicsItemGroup,
+    QGraphicsPolygonItem,
+    QGraphicsLineItem,
+    QGraphicsTextItem
+)
+from PyQt5.QtGui import QBrush, QColor, QPen, QPolygonF, QFont
 from PyQt5.QtCore import QPointF
 from PyQt5.QtCore import Qt
 from .latex_editor import LatexEditor
 
 RADIO_PERTURBACION = 10
+LONGITUD_FLECHA = 10
 
-class PerturbacionVisual(QGraphicsEllipseItem):
+class PerturbacionVisual(QGraphicsItemGroup):
     def __init__(self, perturbacion_back, drawing_area):
-        super().__init__(0, 0, 2 * RADIO_PERTURBACION, 2 * RADIO_PERTURBACION)
+        super().__init__()
         self.perturbacion_back = perturbacion_back
         self.drawing_area = drawing_area
         self.setAcceptHoverEvents(True)
-        self.setBrush(QBrush(QColor("#FFD700")))
-        self.setPen(QPen(Qt.black, 2))
         self.setZValue(1)
         self.setFlag(QGraphicsItem.ItemIsSelectable)
+        self.setFlag(QGraphicsItem.ItemIsFocusable)
         self.setCursor(Qt.PointingHandCursor)
+
+        self.circulo = QGraphicsEllipseItem(0, 0, 2 * RADIO_PERTURBACION, 2 * RADIO_PERTURBACION)
+        self.circulo.setBrush(QBrush(QColor("#FFD700")))
+        self.circulo.setPen(QPen(Qt.black, 2))
+        
+        self.cruz1 = QGraphicsLineItem(4, 4, 2 * RADIO_PERTURBACION - 4, 2 * RADIO_PERTURBACION - 4)
+        self.cruz2 = QGraphicsLineItem(4, 2 * RADIO_PERTURBACION - 4, 2 * RADIO_PERTURBACION - 4, 4)
+        self.cruz1.setPen(QPen(Qt.black, 2))
+        self.cruz2.setPen(QPen(Qt.black, 2))
+
+        puntos_flecha = QPolygonF([
+            QPointF(0, 0),  # Punta de la flecha
+            QPointF(-5, -10),  # Esquina izquierda
+            QPointF(5, -10)   # Esquina derecha
+        ])
+        self.flecha = QGraphicsPolygonItem(puntos_flecha)
+        self.flecha.setBrush(QBrush(Qt.black))
+        self.flecha.setPen(QPen(Qt.black, 2))
+        self.flecha.setPos(RADIO_PERTURBACION, -LONGITUD_FLECHA + RADIO_PERTURBACION)
+        
+        self.linea_flecha = QGraphicsPolygonItem(QPolygonF([
+            QPointF(RADIO_PERTURBACION, -2 * RADIO_PERTURBACION - LONGITUD_FLECHA),
+            QPointF(RADIO_PERTURBACION, 0)
+        ]))
+        self.linea_flecha.setPen(QPen(Qt.black, 2))
+
+        font = QFont()
+        font.setBold(True)
+        self.mas_arriba = QGraphicsTextItem("+")
+        self.mas_arriba.setFont(font)
+        self.mas_arriba.setPos(RADIO_PERTURBACION - 15, -2 * RADIO_PERTURBACION - LONGITUD_FLECHA)
+        self.mas_izquierda = QGraphicsTextItem("+")
+        self.mas_izquierda.setFont(font)
+        self.mas_izquierda.setPos(-13, RADIO_PERTURBACION - 5)
+
+        self.addToGroup(self.circulo)
+        self.addToGroup(self.cruz1)
+        self.addToGroup(self.cruz2)
+        self.addToGroup(self.flecha)
+        self.addToGroup(self.linea_flecha)
+        self.addToGroup(self.mas_arriba)
+        self.addToGroup(self.mas_izquierda)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Delete:
+            self.eliminar_perturbacion()
+        super().keyPressEvent(event)
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
+            self.setFocus()
             self.editar_perturbacion()
         elif event.button() == Qt.RightButton:
             self.mostrar_menu_contextual(event)
@@ -68,6 +132,7 @@ class PerturbacionVisual(QGraphicsEllipseItem):
         buttons.addWidget(cancel_button)
         layout.addLayout(buttons)
 
+        dialog.setStyleSheet("background-color: #333; color: white;")
         dialog.setLayout(layout)
 
         ok_button.clicked.connect(dialog.accept)
