202d2e2f3617c922ad2354520c6469232b1851c6
Pedro Torales <pedroimanoltorales@gmail.com>
2024-10-18 11:24:18 -0300

    mas y menos se ven bien ahora

--- src/ui/base/punto_suma.py
+++ src/ui/base/punto_suma.py
@@ -1,21 +1,10 @@
-import os
-from .macro_vista import MacroVista
-from PyQt5 import QtCore, QtGui
+
 from PyQt5.QtWidgets import (
     QGraphicsEllipseItem, 
-    QMenu, 
-    QAction, 
-    QMessageBox,
-    QGraphicsItem, 
     QGraphicsItemGroup,
-    QGraphicsPolygonItem,
     QGraphicsLineItem,
-    QGraphicsTextItem, 
 )
-from PyQt5.QtGui import QBrush, QColor, QPen, QPolygonF, QFont
-from PyQt5.QtCore import QPointF
-from PyQt5.QtCore import Qt
-from PyQt5 import QtGui
+from PyQt5.QtGui import QBrush, QColor, QPen
 from math import pi, cos, sin
 
 FONDO_COLOR = QColor("#A8DADC")
@@ -37,33 +26,32 @@ class PuntoSuma(QGraphicsItemGroup):
         self.cruz2 = QGraphicsLineItem(x_medio - cruz_cos, y_medio + cruz_sen, x_medio + cruz_cos, y_medio - cruz_sen)
         self.cruz1.setPen(QPen(BORDE_COLOR, 4))
         self.cruz2.setPen(QPen(BORDE_COLOR, 4))
-
+        
 
         cruz_cos_2 = RADIO_PERTURBACION*2 * cos(angle)
         cruz_sen_2 = RADIO_PERTURBACION*2 * sin(angle)
 
+        centro_mas_x = x_medio - cruz_cos_2
+        centro_mas_y = y_medio - cruz_sen_2
+
+        centro_menos_x = x_medio + cruz_cos_2
+        centro_menos_y = y_medio + cruz_sen_2
 
 
-        font = QFont()
-        font.setBold(True)
-        font.setPointSize(10)  # Adjust the size as needed
-        self.mas_arriba = QGraphicsTextItem("+")
-        self.mas_arriba.setDefaultTextColor(BORDE_COLOR)
-        self.mas_arriba.setFont(font)
-        text_rect = self.mas_arriba.boundingRect()
+        self.mas_horizontal = QGraphicsLineItem(centro_mas_x - RADIO_PERTURBACION/2, centro_mas_y, centro_mas_x + RADIO_PERTURBACION/2, centro_mas_y)
+        self.mas_vertical = QGraphicsLineItem(centro_mas_x , centro_mas_y-RADIO_PERTURBACION/2, centro_mas_x , centro_mas_y+RADIO_PERTURBACION/2)
+        self.mas_horizontal.setPen(QPen(BORDE_COLOR, 4))
+        self.mas_vertical.setPen(QPen(BORDE_COLOR, 4))
 
-        self.mas_arriba.setPos(x_medio - text_rect.width()/1.5-RADIO_PERTURBACION, y_medio - text_rect.height()/1.5-RADIO_PERTURBACION)
+        self.menos = QGraphicsLineItem(centro_menos_x-RADIO_PERTURBACION/2, centro_menos_y, centro_menos_x+RADIO_PERTURBACION/2, centro_menos_y)
+        self.menos.setPen(QPen(BORDE_COLOR, 4))
 
-        self.mas_izquierda = QGraphicsTextItem("-")
-        self.mas_izquierda.setFont(font)
-        self.mas_izquierda.setDefaultTextColor(BORDE_COLOR)
-        self.mas_izquierda.setPos(x_medio+RADIO_PERTURBACION/2, y_medio)
-
+        self.addToGroup(self.mas_horizontal)
+        self.addToGroup(self.mas_vertical)
+        self.addToGroup(self.menos)
         self.addToGroup(self.circulo)
         self.addToGroup(self.cruz1)
         self.addToGroup(self.cruz2)
-        self.addToGroup(self.mas_arriba)
-        self.addToGroup(self.mas_izquierda)
         
 
         
